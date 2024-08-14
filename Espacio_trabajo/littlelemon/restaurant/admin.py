9783fc615e2f55555ea76cc1d10d17a1115fc2d3
unknown <tiagosanabria03@gmail.com>
2024-08-13 20:20:40 -0500

    Se hace modificacion de la carpeta Models.py definiendo 2 tablas para base de datos, Reservas y Menus

--- Espacio_trabajo/littlelemon/restaurant/admin.py
+++ Espacio_trabajo/littlelemon/restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Reservas, Menus
 
 # Register your models here.
+admin.site.register(Reservas)
+admin.site.register(Menus)
--- Espacio_trabajo/littlelemon/restaurant/models.py
+++ Espacio_trabajo/littlelemon/restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Reservas(models.Model):
+    id = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateTimeField()
+
+    def __str__(self):
+        return f"Reserva {self.id} para {self.nombre}"
+    
+class Menus(models.Model):
+    id = models.AutoField(primary_key=True)
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits= 10, decimal_places=2)
+    Inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.Title
+    
